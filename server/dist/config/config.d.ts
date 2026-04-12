d758ca1665e3ba11e4d08895b9609b6d0e657558
PrithwiKumarSingh <prithwikumar871@gmail.com>
2026-04-13 00:12:39 +0530

    Remove ignored files from tracking

--- server/dist/config/config.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const JWT_PASSWORD = "Prithwi@123";
-//# sourceMappingURL=config.d.ts.map
--- server/dist/db.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const main: () => Promise<void>;
-//# sourceMappingURL=db.d.ts.map
